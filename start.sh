fbde768c59ac5165728c002f475a6e18e81535c4
Harman Singh <harman28@gmail.com>
2026-03-28 10:06:42 +0100

    fix startup command

--- start.sh
+++ start.sh
@@ -21,4 +21,4 @@ with app.app_context():
         print("Migration done")
 EOF
 
-gunicorn "app:create_app()" --bind "0.0.0.0:${PORT:-8000}"
+gunicorn "app:create_app()" --bind "0.0.0.0:${PORT:-8000}" --chdir backend
